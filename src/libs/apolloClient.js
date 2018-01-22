81129ebc776e5a5efb8e579d73b47699c8cbfff3
Andrés Franco <hello@andresfran.co>
2018-01-22 11:51:53 +0100

    Apollo url switch

--- src/libs/apolloClient.js
+++ src/libs/apolloClient.js
@@ -2,9 +2,12 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+const isProduction = process.env.NODE_ENV === 'production'
+const uri = isProduction ? 'http://graphql.onerights.com' : 'http://localhost:5000'
+
 export default new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:5000'
+    uri,
   }),
   cache: new InMemoryCache()
 });
